05bf936085ce71e8dc6e2d1805016df245845e58
Gabriel Clay <gabrielwclay@gmail.com>
2024-11-23 11:51:37 +1100

    Started setting up core components and interfaces for scene system

--- Source/Scene.h
+++ Source/Scene.h
@@ -1,4 +1,35 @@
 #ifndef __SCENE__
 #define __SCENE__
 
+#include "ComponentObjects.h"
+
+typedef struct SaveStack SaveStack;
+typedef struct TreeObjectRef TreeObjectRef;
+
+struct SaveStack
+{
+
+};
+
+struct TreeObjectRef
+{
+    void *Object;
+    ComponentData *ComponentData;
+};
+
+INTERFACE_DECLARE(Saveable, 
+    int (*Save)(void *object, const ComponentData *component, SaveStack *saveStack);
+    int (*Load)(void *object, const ComponentData *component, SaveStack *saveStack);
+)
+
+COMPONENT_DECLARE(TreeObject, 
+    COMPONENT_IMPLEMENTS_DECLARE(TreeObject, Saveable),
+    COMPONENT_USES_DECLARE(TreeObject, Saveable),
+    TreeObjectRef Parent;
+    
+    size_t ChildCount;
+    size_t ChildListLength;
+
+)
+
 #endif
