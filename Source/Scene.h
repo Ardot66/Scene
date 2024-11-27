f0c83319fc433fdd19d2f3b7b3dadff7cea842d3
Gabriel Clay <gabrielwclay@gmail.com>
2024-11-27 14:39:25 +1100

    Set up several components and interfaces for basic scene function

--- Source/Scene.h
+++ Source/Scene.h
@@ -1,35 +1,54 @@
 #ifndef __SCENE__
 #define __SCENE__
 
+#include "pthread.h"
 #include "ComponentObjects.h"
 
-typedef struct SaveStack SaveStack;
-typedef struct TreeObjectRef TreeObjectRef;
+typedef struct SaveData SaveData;
+typedef struct ObjectRef ObjectRef;
 
-struct SaveStack
+struct SaveData
 {
 
 };
 
-struct TreeObjectRef
+struct ObjectRef
 {
     void *Object;
     ComponentData *ComponentData;
 };
 
 INTERFACE_DECLARE(Saveable, 
-    int (*Save)(void *object, const ComponentData *component, SaveStack *saveStack);
-    int (*Load)(void *object, const ComponentData *component, SaveStack *saveStack);
+    int (*Save)(void *object, const ComponentData *component, SaveData *saveStack);
+    int (*Load)(void *object, const ComponentData *component, SaveData *saveStack);
 )
 
-COMPONENT_DECLARE(TreeObject, 
-    COMPONENT_IMPLEMENTS_DECLARE(TreeObject, Saveable),
-    COMPONENT_USES_DECLARE(TreeObject, Saveable),
-    TreeObjectRef Parent;
+INTERFACE_DECLARE(Readyable,
+    int (*Ready)(void *object, const ComponentData *component);
+    int (*Exit)(void *object, const ComponentData *component);
+)
+
+COMPONENT_DECLARE(Node, 
+    ,
+    COMPONENT_USES_DECLARE(Node, Readyable)
+    COMPONENT_USES_DECLARE(Node, Saveable),
+    ObjectRef Parent;
+    ObjectRef ThreadGroup;
     
     size_t ChildCount;
     size_t ChildListLength;
+    ObjectRef *Children;
+)
 
+enum ThreadGroupMode {THREAD_GROUP_MODE_NONE, THREAD_GROUP_MODE_READ, THREAD_GROUP_MODE_WRITE};
+
+COMPONENT_DECLARE(ThreadGroup,
+    COMPONENT_IMPLEMENTS_DECLARE(ThreadGroup, Readyable),
+    ,
+    pthread_mutex_t Mutex;
+    pthread_cond_t Condition;
+    enum ThreadGroupMode Mode;
+    size_t ActiveThreadCount;
 )
 
 int FreeQueueInit();
